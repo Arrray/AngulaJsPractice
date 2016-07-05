152fd767de5b9dc9b00b68ac8be2b0700258563e
高军超 <gjc@hrsoft.net>
2016-07-05 17:58:15 +0800

    极客学院AngulaJS表单验证练习-完结

--- practice/AngulaJS_Form/js/main.js
+++ practice/AngulaJS_Form/js/main.js
@@ -3,10 +3,38 @@
  */
 
 angular.module('myApp', [])
-        .controller('MianController', function($scope)
+        .controller('SignUpController', function($scope)
 {
+    $scope.userdata = {};
     $scope.submitForm = function ()
     {
-        console.log('表单提交啦!')
+        console.log($scope.userdata);
+        if($scope.signUpForm.$invalid){
+            alert('请检出用户信息');
+        }else {
+            alert('提交成功!');
+        }
     }
-});
+
+
+})
+
+    .directive('compare', function () {
+        var o = {};
+        o.strict = 'AE';
+        o.scope = {
+            orgText: '=compare'
+        };
+
+        o.require = 'ngModel';
+        o.link = function(sco,ele,att,con){
+            con.$validators.compare = function(v){
+                return v == sco.orgText;
+            };
+
+            sco.$watch('orgText',function(){
+                con.$validate();
+            })
+        }
+        return o;
+    })
